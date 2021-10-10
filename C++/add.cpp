be9987105e2930813ce5500ecee421520ef2350c
kumar21-crypto <72235150+kumar21-crypto@users.noreply.github.com>
2021-10-10 09:09:35 +0530

    Create add.cpp file
    I created add.cpp file under C++ folder for add two integers.

---
+++ C++/add.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+  int a,b;
+  cout<<"enter two numbers"<<endl;
+  cin>>a>>b>>;
+
+  cout<<"the sum of a and b is "<< a + b <<endl;
+
+   return 0;
+
+}
