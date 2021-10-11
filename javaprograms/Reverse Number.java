6b7b0be8767838babe85fa8af3bc39f4332d7112
Priyanshu Vaishnavi <vaishnavipriyanshu198@gmail.com>
2021-10-11 19:57:12 +0530

    added_programs

---
+++ javaprograms/Reverse Number.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class ReverseNumberWhile
+{
+   public static void main(String args[])
+   {
+      int num=0;
+      int reversenum =0;
+      System.out.println("Input your number and press enter: ");
+      //This statement will capture the user input
+      Scanner in = new Scanner(System.in);
+      //Captured input would be stored in number num
+      num = in.nextInt();
+      //While Loop: Logic to find out the reverse number
+      while( num != 0 )
+      {
+          reversenum = reversenum * 10;
+          reversenum = reversenum + num%10;
+          num = num/10;
+      }
+
+      System.out.println("Reverse of input number is: "+reversenum);
+   }
+}
