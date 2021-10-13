fde227b1c037d32ac18717e0ebe199c6aee3a15e
shivam <71328785+aditikhandare4@users.noreply.github.com>
2021-10-13 11:27:46 +0530

    Display calendar of given month of the year

---
+++ python/calender.py
@@ -0,0 +1,11 @@
+# Python program to display calendar of
+# given month of the year
+   
+# import module
+import calendar
+   
+yy = 2021
+mm = 10
+   
+# display the calendar
+print(calendar.month(yy, mm)
